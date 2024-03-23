6138800f82f1412a5aab374a6a52ab2f72a27385
AmiraslanliAF <arkan.amiraslanli@kapitalbank.az>
2024-03-23 14:26:04 +0400

    added

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,13 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class SDK {
+    public init() { }
+}
+
+public extension SDK {
+    var doSmthng: Void {
+        print("called")
+        return
+    }
+}
