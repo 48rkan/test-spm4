3d812b20e00ba7d5004fa21e8482489d321626a2
AmiraslanliAF <arkan.amiraslanli@kapitalbank.az>
2024-03-23 14:50:02 +0400

    added

---
+++ Sources/MyLibrary55/File 2.swift
@@ -0,0 +1,19 @@
+//
+//  File 2.swift
+//  
+//
+//  Created by Amiraslanli Arkan on 23.03.24.
+//
+
+import Foundation
+
+public class SDK3 {
+    public init() { }
+}
+
+public extension SDK3 {
+    var doSmthng: Void {
+        print("called2")
+        return
+    }
+}
