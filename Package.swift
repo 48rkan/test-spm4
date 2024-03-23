2cdf5c7f031168b45921d363febfbec08671641c
AmiraslanliAF <arkan.amiraslanli@kapitalbank.az>
2024-03-23 14:40:00 +0400

    added

--- Package.swift
+++ Package.swift
@@ -4,20 +4,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
+    name: "MyLibrary55",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
+            name: "MyLibrary55",
+            targets: ["MyLibrary55"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyLibrary"),
-        .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
+            name: "MyLibrary55"),
     ]
 )
